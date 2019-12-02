4fedcfa0866f2bb8e569c4b8fd0f6bd21e395fca
anshjainyo <58123644+anshjainyo@users.noreply.github.com>
2019-12-02 06:44:31 +0530

    My First Personal Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var description = new Typed('#description', {
+        strings: ['Tech Enthusiast^2500', 'Web Developer^2500', 'Data Analyst^2500'],
+        shuffle: true,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 100,
+        cursorChar: '_',
+    })
+})
