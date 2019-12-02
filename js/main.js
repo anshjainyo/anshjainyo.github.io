92dcdb2341bacfd17acd15f7ff6aac30d08054cb
anshjainyo <58123644+anshjainyo@users.noreply.github.com>
2019-12-02 15:25:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var description = new Typed('#description', {
-        strings: ['Tech Enthusiast^2500', 'Web Developer^2500', 'Data Analyst^2500'],
+        strings: ['Tech Enthusiast^2500', 'Web Developer^2500', 'Music Lover^2500' , 'App Developer^2500' , 'Java Developer^2500'],
         shuffle: true,
         loop: true,
         typeSpeed: 100,
